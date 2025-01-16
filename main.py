ffadcc32ca7e3daeaa20f4291ea0ee885034658b
Joey Rudek <jrudek@ucsd.edu>
2025-01-15 23:55:04 -0800

    repo reorganizing

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
+
 def main():
     import parse_cards
 
-    cardfile = 'base_game.cards'
+    cardfile = 'cards/base_game.cards'
 
     cards = parse_cards.from_file(cardfile)
-
+    
     ages = [0 for _ in range(11)]
 
     for c in cards:
@@ -14,6 +15,9 @@ def main():
     for i in range(1, 11):
         print(f"Cards of age {i}: {ages[i]}")
 
+    for c in cards[:15]:
+        print(c)
+        input()
 
 
 if __name__=="__main__":
--- parse_cards.py
+++ parse_cards.py
@@ -11,70 +11,17 @@ from game_state import Color, Icon
 from game_state import Card
 from dogma_behavior import DogmaAction, DEffect
 
+from utils.lark_utils import find_unused_rules
 
-class CardBuilder(Transformer):
-
-    # TODO: separate Transformer class for DogmaBuild-ing?
-
-    def deffect_grammar__NUM(self, number):
-        try:
-            return int(number)
-        except ValueError:
-            if number in ('a', 'an'):
-                return 1
-            number_terms = ('zero', 'one', 'two', 'three', 'four')
-            if number not in number_terms:
-                raise ValueError(f"Couldn't parse number term {number}")
-            return number_terms.index(number)
-
-    def deffect_grammar__common__AGE_VAL(self, age_val): 
-        return self.common__AGE_VAL(age_val)
-    
-    def common__AGE_VAL(self, age_val):
-        if age_val == 'counter':
-            return 'counter'
-        if age_val == 'plus-one':
-            return 'plus-one'
-        return int(age_val)
-    
-    def deffect_grammar__effect_header(self, items):
-        is_demand : bool = ('Shared', 'Demand').index(items[0]) == 1
-        key_icon : Icon = Icon[str(items[1]).upper()]
-        return (is_demand, key_icon)
-    
-    def deffect_grammar__stmts(self, items):
-        return tuple(items)
-    
-    def deffect_grammar__stmt(self, items):
-        stmt = items[0]
-        args = tuple(stmt.children)
-        return DogmaAction(stmt.data[len('deffect_grammar__'):], args)
-    
-    def icons(self, items):
-        return [Icon[str(i).upper()] for i in items]
-    
-    def dogma(self, items):  # list of DEffects
-        return items
-    
-    def dogma_effect(self, items):  # one DEffect
-        return DEffect(
-            is_demand=items[0][0], key_icon=items[0][1], effects=items[1])
-
-    def card(self, items):
-        #card_name, age, color, icons, dogma = items
-        card_name = str(items[0])
-        age = items[1]
-        color = Color[str(items[2]).upper()]
-        icons = items[3]
-        dogma = items[4]
-        return Card(card_name, color, age, icons, dogma)    
-
-    def cards(self, items): return items
+# TODO: interpret!
 
 
-def from_file(filename : str) -> List:
+def from_file(filename : str):  # todo typing
     with open(filename) as f:
         cards_txt = f.read()
-    parser = Lark.open("card_grammar.lark", start="cards")
+    parser = Lark.open("grammars/deffect_grammar.lark", start="cards")
     tree = parser.parse(cards_txt)
-    return CardBuilder().transform(tree)
+    print(f'Unused rules: {find_unused_rules(tree, ("grammars/common", "grammars/deffect_grammar"), "cards")}')
+    input("(press any key...)")
+    return tree
+    #return CardBuilder().transform(tree)
